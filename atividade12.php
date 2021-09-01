f893408e8cedf8c04edc7a55485a43e18ba3f0d8
igorbcorradi <57455929+igorbcorradi@users.noreply.github.com>
2021-09-01 10:23:21 -0300

    Add files via upload

---
+++ atividade12.php
@@ -0,0 +1,24 @@
+<?php
+
+print ("1) Use array com construtor <br/>");
+$vet = array("Igor", "Rafael", "Guilherme");
+print_r($vet);
+
+print ("<br/>");
+print ("<br/>");
+
+print ("2) Use matriz com construtor <br/>");
+$matriz = Array(Array("1", "2", "3", "4"), Array("Igor", "Rafael", "Guilherme"));
+print_r($matriz);
+
+print ("<br/>");
+print ("<br/>");
+
+print ("3) Use array com implícito <br/>");
+$vet1 = ["5", "4", "3", "2"];
+print_r($vet1);
+
+print ("<br/>");
+print ("<br/>");
+
+?>
